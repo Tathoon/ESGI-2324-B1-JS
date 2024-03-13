1d83cda9ccf3f9dde1f7ab1b751e71e31c685fac
Thomas <andrioletti.thomas@yahoo.com>
2024-03-13 23:06:09 +0100

    Films Stars Wars + description personnages

--- Projets/index.js
+++ Projets/index.js
@@ -117,8 +117,83 @@ RemoveItem.addEventListener("click", function() {
     document.getElementById("Panier").innerHTML = ""
 })
 
+let promesse = new Promise((resolve, reject) => {
+    let condition = true
+    if (condition) {
+        setTimeout(() => resolve("Opératon réussie"), 1000)
+    } else {
+        reject("Opération échouée")
+    }
+    })
+    
+function getFilmDescription(filmNumber) {
+    fetch(`https://swapi.dev/api/films/${filmNumber}/`)
+    .then(response => {
+        if (!response.ok) {
+              throw new Error('Erreur (films)');
+            }
+            return response.json();
+          })
+          .then(filmData => {
+            // recupere les détails des persos de chaque film
+            getCharactersDetails(filmData.characters);
+    
+            // met à jour le contenu de filmDescription
+            document.getElementById('filmDescription').innerHTML = `
+              <h2>${filmData.title}</h2>
+              <p>${filmData.opening_crawl}</p>
+            `
+          })
+      }
+    
+      // recupere et affiche les détails des persos
+function getCharactersDetails(characterURLs) {
+    characterURLs.forEach(url => {
+        fetch(url)
+            .then(response => {
+              if (!response.ok) {
+                throw new Error('Erreur (persos)');
+              }
+              return response.json();
+            })
+            .then(characterData => {
+              // ajoute les details du persos a filmDescription
+              document.getElementById('filmDescription').innerHTML += `
+                <h2>${characterData.name}</h2>
+                <p><strong>Genre : </strong>${characterData.gender}</p>
+                <p><strong>Année de naissance : </strong>${characterData.birth_year}</p>
+                <hr>
+              `
+            })
+        })
+      }
+    
+      // associe la fonction à chaque bouton
+document.getElementById('film1').addEventListener('click', function () {
+    getFilmDescription(1);
+});
+    
+document.getElementById('film2').addEventListener('click', function () {
+    getFilmDescription(2);
+});
+    
+document.getElementById('film3').addEventListener('click', function () {
+     getFilmDescription(3);
+});
+    
+document.getElementById('film4').addEventListener('click', function () {
+    getFilmDescription(4);
+});
+    
+document.getElementById('film5').addEventListener('click', function () {
+    getFilmDescription(5);
+});
+    
+document.getElementById('film6').addEventListener('click', function () {
+    getFilmDescription(6);
+});
+
 console.log(addbutton)
 console.log(subbutton)
 console.log(multbutton)
 console.log(divbutton)
-console.log(compteur)
